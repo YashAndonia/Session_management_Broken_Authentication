13ab1e4a907f58885a8b64babe989042e2b3d5d2
YashAndonia <yashnaik2406@gmail.com>
2018-03-27 19:41:20 +0530

    Fixed starting login_byebye error.
    Website now functions smoothly with no login issues.
    Also, the situation of all tabs being closed and directly trying to access Wilkommen_2.php has been solved. All cases are looked over now, in session management, due to the new changhes that involve unset() and session_regenerate in Wilkommen_2.php

--- WEBSITE1(Do)/Solution/Wilkommen_2.php
+++ WEBSITE1(Do)/Solution/Wilkommen_2.php
@@ -1,20 +1,37 @@
 <?php
-
+/*
 session_start();
-
-
- if(!isset($_SESSION["nam"])){//session name is not set so logout
-	 session_regenerate_id();
-$_SESSION["nam"]="DNE";
-header('location:byebye.php');
- }
+session_regenerate_id();
  if(isset($_POST["fname"])){//the form members are set and are yet to be filled into the session variables, so stay loggedin
 $a=$_POST["fname"];
 $b=$_POST["lname"];
 $_SESSION["nam"]=$a." ".$b;
  }
- 
- 
+ else {
+   if(!isset($_SESSION["nam"])){//session name is not set so logout
+
+  header('location:byebye.php');
+   }
+ }*/
+ session_start();
+ if(isset($_POST["fname"]))
+ {
+   $a=$_POST["fname"];
+   $b =$_POST["lname"];
+   session_regenerate_id();
+   $_SESSION["nam"]=$a." ".$b;
+   unset($_POST["fname"]);
+
+ }
+ else {
+
+   if(!isset($_SESSION["nam"]))
+   {
+     header('location:byebye.php');
+   }
+ }
+
+
  ?>
 
 <!DOCTYPE HTML>
@@ -23,27 +40,27 @@ $_SESSION["nam"]=$a." ".$b;
 	<title>Let's Do</title>
 	<link href="dostyle_mainpg.css" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-	
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-</head> 
+</head>
 
 <body>
 
 	<header>
 		<h1 style="width=100px;"><a href="Do_1614093.html">:Do.</a></h1>
-		
+
 		<input name="search" type="text" class="searchbar" placeholder="Search.">
 		</input>
-		 
+
 		<div id="UserInfo">
-			
+
 			<div id="dp"><img class="avatar" src="https://www.timeshighereducation.com/sites/default/files/styles/the_breaking_news_image_style/public/jojohnson2_0.jpg?itok=L6J5MbTL" height=75 width=auto/></div>
-			
+
 				<h2 ><?php echo $_SESSION["nam"];?></h2>
-		
-		
+
+
 			<div id="panel" style=" top:0px;">
 				<h2>|Settings</h2>
 				<h2>|Requests</h2>
@@ -51,83 +68,83 @@ $_SESSION["nam"]=$a." ".$b;
 				<h2>|About Us</h2>
 				<a href="\logouter.php"><h2>|Log Out</h2></a>
 			</div>
-		
+
 		</div>
-		
+
 	</header>
 <br>
 <br>
 	<div id="Categories">
 		<br><br><h2>Categories.</h2>
-		
-		
+
+
 		<table style="top:60%;">
-		
+
 			<tr>
 				<td>Sports</td>
-				
+
 			</tr>
 			<tr>
 				<td>Food</td>
-				
+
 			</tr>
 			<tr>
 				<td>Cultutre</td>
-				
+
 			</tr>
-			
+
 			<tr>
 				<td>Arts</td>
-				
+
 			</tr>
-			
+
 			<tr>
 				<td>Literature</td>
-				
+
 			</tr>
 			<tr>
 				<td>Festivals</td>
-				
+
 			</tr>
-			
+
 		</table>
-		
+
 	</div>
-	
+
 	<div id="map" style="top:35%;">
 	<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15083.26482820217!2d72.89888315869142!3d19.071816795943672!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1519173083915" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-	
+
 	</div>
-	
+
 	<div class="stream" id="fir">
-	
+
 	<h2 class="title "><a href="1614093_Event_do.php" style="color:white;">College Cricket Tournament</h2></a>
-	
-	
+
+
 	</div>
-	
+
 	<div class="stream" id="sec">
-	
+
 	<h2 class="title">Silent DJ!</h2>
-	
-	
+
+
 	</div>
-	
+
 	<div class="stream" id="third">
-	
+
 	<h2 class="title">Food Truck Festival</h2>
-	
-	
+
+
 	</div>
-	
+
 	<div class="stream" id="fourth">
-	
+
 	<h2 class="title">Beach Party</h2>
-	
-	
+
+
 	</div>
-	
-	
+
+
 	<footer style="position:fixed; bottom:0px;">
 	Session_ID:<?php echo session_id();?>
 	</footer>
@@ -136,7 +153,7 @@ $_SESSION["nam"]=$a." ".$b;
 
 function logout(){
 	return true;
-	
+
 }
 
 </script>
